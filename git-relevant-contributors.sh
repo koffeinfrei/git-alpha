15306323740dbd60f8898bdd62087fad0c6d6f57
Alexis Reigel <mail@koffeinfrei.org>
2015-02-16 23:47:42 +0100

    support utf-8 file names

--- git-relevant-contributors.sh
+++ git-relevant-contributors.sh
@@ -18,7 +18,7 @@ total_lines() {
 # Gets all the source files in the repository
 # (excluding bindary files)
 source_files() {
-  git ls-files $start_dir | xargs -n1 grep -Il -d skip .
+  git ls-files -z $start_dir | xargs -0 grep -Il -d skip .
 }
 
 # Gets all blamed lines of all source files in the repository
